a471201dd7c36d70e1ea30963fe112e7578eba8c
olegDk <oleg.davydiuk1995@gmail.com>
2021-08-05 18:15:43 +0300

    Adjust market data json format

--- emulator/config/constants.py
+++ emulator/config/constants.py
@@ -1,58 +1,70 @@
-INIT_PCT = -1e3
-SYMBOL = 'symbol'
-ORDER = 'order'
-ORDER_REQUEST = 'orderRequest'
-ORDER_RELATED_DATA = 'orderRelatedData'
-DATETIME = "datetime"
-DATA = 'data'
-PCT_BID_NET = 'pctBidNet'
-PCT_ASK_NET = 'pctAskNet'
-BID = 'bid'
-ASK = 'ask'
-BID_VENUE = 'bidVenue'
-ASK_VENUE = 'askVenue'
-PRICE = 'limit'
-SIDE = 'side'
-SIZE = 'size'
-VENUE = 'venue'
-REF = 'refNo'
-MODEL = 'model'
-CID = 'cid'
-TARGET = 'target'
+# Connection constants
+RABBIT_MQ_HOST = 'localhost'
+RABBIT_MQ_PORT = 5672
+CLIENT_VERSION_KEY = 'clientVersion'
 ACCOUNT_ID = 'WBTE'
 VERSION_OF_APP = '1.2.3.4'
 TIMEOUT_GRACE_PERIOD = 180
 RECONNECTION_FREQUENCY = 5
 N_ATTEMPTS = 10
 ERROR = 'error'
-SESSION_KEY = 'sessionKey'
-ACCOUNT_ID_KEY = 'accountId'
 VERSION_OF_APP_KEY = 'clientVersion'
 SEQ = 'seqNo'
+REF = 'refNo'
 LOGON_TYPE = 'logon'
+SESSION_KEY = 'sessionKey'
+SYMBOLS = 'symbols'
+
+# Stock info constants
+SYMBOL = 'symbol'
+PCT_BID_NET = 'pctBidNet'
+PCT_ASK_NET = 'pctAskNet'
+BID = 'bid'
+ASK = 'ask'
+BID_VENUE = 'bidVenue'
+ASK_VENUE = 'askVenue'
+CLOSE = 'close'
+L1 = 'l1'
+INIT_PCT = -1e3
+
+# Message types
+MESSAGE_ID = 'messageId'
+ORDER_REQUEST = 'orderRequest'
 MARKET_DATA_TYPE = 'marketData'
 ORDER_STATUS = 'orderStatus'
 ORDER_RESPONSE = 'orderResponse'
-RESPONSE = 'response'
-ORDER_DATA = 'orderData'
-MESSAGE_ID = 'messageId'
-SYMBOLS = 'symbols'
-STATUS = 'status'
-RABBIT_MQ_HOST = 'localhost'
-RABBIT_MQ_PORT = 5672
-TICKER = 'ticker'
 SUBSCRIBE = 'subscribe'
-CLIENT_VERSION_KEY = 'clientVersion'
 KEEP_ALIVE = 'keepAlive'
+
+# Plain constants to use in code
+MODEL = 'model'
+TARGET = 'target'
+DATETIME = "datetime"
+
+# Constants related to orderRequest only
 TIF = 'TIF'
 LIMIT = 'limit'
-EXTENDED = 5
-ORDER_ID = 'orderId'
-CLIENT_ID = 'clientId'
-TIME = 'time'
+EXTENDED = 'TIF_GTX'  # or 1 if integer
+CID = 'cid'
+
+# Constants related to orderResponse only
 MNEMONIC = 'mnemonic'
 PENDING_SIZE = 'pendingSize'
 BPU = 'BPU'
 CPNL = 'CPNL'
-ID = 'id'
-CLOSE = 'close'
+ORDER_ID = 'orderId'
+TIME = 'time'
+STATUS = 'status'
+RESPONSE = 'response'
+CLIENT_ID = 'clientId'
+
+# Order request-response data
+ORDER = 'order'
+ORDER_RELATED_DATA = 'orderRelatedData'
+DATA = 'data'
+PRICE = 'limit'
+SIDE = 'side'
+SIZE = 'size'
+VENUE = 'venue'
+ACCOUNT_ID_KEY = 'accountId'
+ORDER_DATA = 'orderData'
