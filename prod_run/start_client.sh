15512f4b00d933c468261aae46698f1ed566ae8d
olegDk <oleg.davydiuk1995@gmail.com>
2021-08-30 15:39:32 +0300

    hotfix

--- prod_run/start_client.sh
+++ prod_run/start_client.sh
@@ -1,6 +1,6 @@
 cd ~/home/takion_trader/algotrading
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate takion_trader
-python main.py '10.101.3.83' #'192.168.137.11' '91.219.61.233'
+python main.py '192.168.137.11' '91.219.61.233' #'10.101.3.83'
 conda deactivate
 cd ~
