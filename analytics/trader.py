ee7697892c54e9cf6531ef389c6accc9d686f6ee
olegDk <oleg.davydiuk1995@gmail.com>
2021-09-16 13:42:54 +0300

    upd blacklist

--- analytics/trader.py
+++ analytics/trader.py
@@ -459,13 +459,8 @@ class Trader:
 
     def __init_policy(self):
         traidable_stocks = list(self.__stock_to_sector.keys())
-        black_list = ['CTXS',
-                      'RIG',
-                      'SE',
-                      'CVX',
-                      'BB',  # Remove altogether in next modeling process
-                      'BILI',
-                      'GS']  # Add untraidable stocks here
+        black_list = ['BB',
+                      'CS']  # Add untraidable stocks here
         policy_dict = {APPLICATION_SOFTWARE: NEUTRAL,
                        BANKS: NEUTRAL,
                        OIL: NEUTRAL,
