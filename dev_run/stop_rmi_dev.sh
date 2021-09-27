cd69b71ae425d24922d60d2a33760adfedecd4a2
olegDk <oleg.davydiuk1995@gmail.com>
2021-09-27 15:25:07 +0300

    hotfix

--- dev_run/stop_rmi_dev.sh
+++ dev_run/stop_rmi_dev.sh
@@ -3,5 +3,5 @@ docker rm $(docker ps -a -q)
 docker image rm takion_trader_receive_market_data
 docker image rm takion_trader_receive_order_related_data
 docker image rm takion_trader_inference_runner
-docker image rm takion_trader_receive_news
+#docker image rm takion_trader_receive_news
 docker image rm redis
