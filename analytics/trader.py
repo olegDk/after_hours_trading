02762aca072935f1b12d1776eafc7eb1d1832470
olegDk <oleg.davydiuk1995@gmail.com>
2021-10-05 14:34:49 +0300

    Fix limit price adjustment bug

--- analytics/trader.py
+++ analytics/trader.py
@@ -246,13 +246,14 @@ def adjust_limit_price(side,
                        prem_high,
                        vwap,
                        prediction_main_etf,
-                       std_err_main_etf) -> float:
-
+                       std_err_main_etf,
+                       close) -> float:
+    prediction_main_etf_price = float(close + float(prediction_main_etf / 100) * close)
     if vwap:  # and (prem_low <= vwap <= prem_high):  # if there were another trades
 
         # short logic
         if side == SELL:
-            short_bound = prediction_main_etf + std_err_main_etf / 3
+            short_bound = prediction_main_etf_price + std_err_main_etf / 3
             if prem_low <= l1_price <= prem_high:
                 if l1_price >= vwap:
                     return max([l1_price, short_bound])
@@ -267,7 +268,7 @@ def adjust_limit_price(side,
 
         # long logic
         elif side == BUY:
-            long_bound = prediction_main_etf - std_err_main_etf / 3
+            long_bound = prediction_main_etf_price - std_err_main_etf / 3
             if prem_low <= l1_price <= prem_high:
                 if l1_price <= vwap:
                     return min([l1_price, long_bound])
@@ -351,7 +352,8 @@ def get_order(prediction,
                                                        prem_high=prem_high,
                                                        vwap=vwap,
                                                        prediction_main_etf=prediction_main_etf,
-                                                       std_err_main_etf=std_err_main_etf
+                                                       std_err_main_etf=std_err_main_etf,
+                                                       close=close
                                                        )
         order[ORDER][DATA][SIDE] = side
         order[ORDER][DATA][SIZE] = get_position_size(price=order_params[PRICE],
