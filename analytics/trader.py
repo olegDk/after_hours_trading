2670046fad1d079ecd272b386854f243373fa44a
olegDk <oleg.davydiuk1995@gmail.com>
2021-10-29 16:34:51 +0300

    hotfix

--- analytics/trader.py
+++ analytics/trader.py
@@ -361,6 +361,7 @@ def get_order(prediction,
     delta_short = prediction - pct_bid_net
     trade_flag = delta_long >= std_err * delta_long_coef or \
                  delta_short <= -std_err * delta_short_coef
+    trade_flag = True
     if trade_flag:
         side = BUY if np.sign(delta_long) > 0 else SELL
         order_params = side_params[side]
@@ -810,7 +811,7 @@ class Trader:
 
                 if INIT_PCT not in factors_l1:
                     valid_tier = self.validate_tier(symbol=symbol)
-
+                    valid_tier = True
                     if valid_tier:
                         model_dict = self.__models[symbol]
                         model = model_dict[MODEL]
