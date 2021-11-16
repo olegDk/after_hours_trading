777d298743a6c011bd4a9c007a59a0ab724a88d4
olegDk <oleg.davydiuk1995@gmail.com>
2021-11-16 16:32:16 +0200

    hotfix

--- analytics/trader.py
+++ analytics/trader.py
@@ -740,7 +740,7 @@ class Trader:
 
                 if INIT_PCT not in factors_l1:
                     valid_tier = self.validate_tier(symbol=symbol)
-
+                    valid_tier = True
                     if valid_tier:
                         model_dict = self.__models[symbol]
                         model = model_dict[MODEL]
@@ -881,6 +881,7 @@ class Trader:
         delta_short = prediction - pct_bid_net
         trade_flag = delta_long >= std_err * delta_long_coef or \
                      delta_short <= -std_err * delta_short_coef
+        trade_flag = True
         if trade_flag:
             print('There is trade flagg1!!!!!!adfasldkfja;sldfjk;alsdkfj;alsdkfj;alskdfj;alksdjf')
             side = BUY if np.sign(delta_long) > 0 else SELL
